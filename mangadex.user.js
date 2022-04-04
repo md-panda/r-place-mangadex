58d9f4e3f7b27480ed2b64c6b0e4abdaf6501798
md-panda <85464892+md-panda@users.noreply.github.com>
2022-04-04 12:43:34 +0200

    MD update

--- mangadex.user.js
+++ mangadex.user.js
@@ -2,10 +2,10 @@
 // @name         Reddit Place - Armée de Kameto
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  On va récuperer ce qui nous est du de droit.
+// @description  MangaDex logo goooooo
 // @author       Adcoss95
 // @match        https://hot-potato.reddit.com/embed*
-// @icon         https://styles.redditmedia.com/t5_4eiiz1/styles/communityIcon_ojy24r8j90o81.jpg
+// @icon         https://mangadex.org/favicon.ico
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -13,9 +13,8 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://github.com/CorentinGC/reddit-place-kcorp/raw/main/overlay.png";
+            i.src = "https://github.com/md-panda/r-place-mangadex/raw/main/overlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
-            console.log(i);
             return i;
         })())
  
